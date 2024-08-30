08c41d5bbdca502648a404368d3d9463d9d90472
Leo Alfred <leojalfred@gmail.com>
2024-08-29 19:10:24 -0500

    Add initial text

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,23 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
-  return <View></View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Memento</Text>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: 'SpaceMono-Regular',
+  },
+})
