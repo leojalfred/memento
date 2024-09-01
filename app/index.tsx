ba70a684787e0e8d2921b1f6eb8e5b367232df84
Leo Alfred <leojalfred@gmail.com>
2024-09-01 13:20:22 -0500

    MEM-3 Animate MediaPicker in and out

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,13 @@
 import { Feather } from '@expo/vector-icons'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { TextInput, TouchableOpacity, View } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import colors from 'tailwindcss/colors'
 import { z } from 'zod'
@@ -10,6 +15,24 @@ import { z } from 'zod'
 const entrySchema = z.object({
   text: z.string(),
 })
+
+function MediaPicker() {
+  return (
+    <View className="flex-row self-start rounded-full border border-gray-700">
+      <TouchableOpacity className="px-4 py-1">
+        <Feather name="image" size={20} color={colors.gray[700]} />
+      </TouchableOpacity>
+      <View className="h-full w-px bg-gray-700" />
+      <TouchableOpacity className="px-4 py-1">
+        <Feather name="video" size={20} color={colors.gray[700]} />
+      </TouchableOpacity>
+      <View className="h-full w-px bg-gray-700" />
+      <TouchableOpacity className="px-4 py-1">
+        <Feather name="mic" size={20} color={colors.gray[700]} />
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default function HomeScreen() {
   const { control } = useForm<z.infer<typeof entrySchema>>({
@@ -19,6 +42,18 @@ export default function HomeScreen() {
   const [selectionStart, setSelectionStart] = useState(0)
   const [selectionEnd, setSelectionEnd] = useState(0)
   const isMediaPickerShown = selectionEnd - selectionStart > 0
+
+  const opacity = useSharedValue(0)
+  useEffect(() => {
+    opacity.value = withTiming(isMediaPickerShown ? 1 : 0, { duration: 200 })
+  }, [isMediaPickerShown, opacity])
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+      display: opacity.value === 0 ? 'none' : 'flex',
+    }
+  })
 
   return (
     <SafeAreaView>
@@ -43,21 +78,9 @@ export default function HomeScreen() {
             />
           )}
         />
-        {isMediaPickerShown && (
-          <View className="flex-row self-start rounded-full border border-gray-700">
-            <TouchableOpacity className="px-4 py-1">
-              <Feather name="image" size={20} color={colors.gray[700]} />
-            </TouchableOpacity>
-            <View className="h-full w-px bg-gray-700" />
-            <TouchableOpacity className="px-4 py-1">
-              <Feather name="video" size={20} color={colors.gray[700]} />
-            </TouchableOpacity>
-            <View className="h-full w-px bg-gray-700" />
-            <TouchableOpacity className="px-4 py-1">
-              <Feather name="mic" size={20} color={colors.gray[700]} />
-            </TouchableOpacity>
-          </View>
-        )}
+        <Animated.View style={animatedStyle}>
+          <MediaPicker />
+        </Animated.View>
       </View>
     </SafeAreaView>
   )
