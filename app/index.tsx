b77d47e20367a4b191e665d3a03297270212f19a
Leo Alfred <leojalfred@gmail.com>
2024-09-01 11:41:00 -0500

    MEM-2 Add form handling and validation

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,45 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { styled } from 'nativewind'
+import { Controller, useForm } from 'react-hook-form'
 import { TextInput } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import colors from 'tailwindcss/colors'
+import { z } from 'zod'
+
+const entrySchema = z.object({
+  text: z.string(),
+})
 
 const StyledTextInput = styled(TextInput)
 
 export default function HomeScreen() {
+  const {
+    control,
+    formState: { errors },
+  } = useForm<z.infer<typeof entrySchema>>({
+    resolver: zodResolver(entrySchema),
+  })
+
   return (
     <SafeAreaView>
-      <StyledTextInput
-        className="mx-4 font-cp"
-        autoFocus={true}
-        multiline={true}
-        placeholder="Start writing..."
-        selectionColor={colors.gray[500]}
-        onSelectionChange={(event) => {
-          const { selection } = event.nativeEvent
-          if (selection.end - selection.start > 0) {
-            console.log(selection)
-          }
-        }}
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <StyledTextInput
+            className="mx-4 font-cp"
+            autoFocus={true}
+            multiline={true}
+            placeholder="Start writing..."
+            selectionColor={colors.gray[500]}
+            value={value}
+            onChangeText={onChange}
+            onSelectionChange={(event) => {
+              const { start, end } = event.nativeEvent.selection
+              if (end - start > 0) console.log(value.substring(start, end))
+            }}
+          />
+        )}
+        name="text"
       />
     </SafeAreaView>
   )
