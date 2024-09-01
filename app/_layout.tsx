25f71505c20bd524ba1044ce78457bcb70a400a1
Leo Alfred <leojalfred@gmail.com>
2024-09-01 00:11:14 -0500

    MEM-5 Add Courier Prime to Tailwind config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,34 @@
-import { DarkTheme, ThemeProvider } from '@react-navigation/native'
+import {
+  CourierPrime_400Regular,
+  CourierPrime_700Bold,
+  useFonts,
+} from '@expo-google-fonts/courier-prime'
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    cp: CourierPrime_400Regular,
+    'cp-bold': CourierPrime_700Bold,
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
-    <ThemeProvider value={DarkTheme}>
-      <Stack>
+    <ThemeProvider value={DefaultTheme}>
+      <Stack screenOptions={{ navigationBarHidden: true }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,14 @@
-import { Platform, StyleSheet, Text } from 'react-native'
+import { styled } from 'nativewind'
+import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+const StyledSafeAreaView = styled(SafeAreaView)
+const StyledText = styled(Text)
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Memento</Text>
-    </SafeAreaView>
+    <StyledSafeAreaView className="items-center justify-center flex-1">
+      <StyledText className="font-cp text-2xl">Memento</StyledText>
+    </StyledSafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontFamily: Platform.select({
-      android: 'CourierPrime_400Regular',
-      ios: 'CourierPrime-Regular',
-    }),
-  },
-})
