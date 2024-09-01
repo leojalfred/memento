875310e550c1c287dcfe5cc63fdb0fc7c4a606fd
Leo Alfred <leojalfred@gmail.com>
2024-09-01 13:13:17 -0500

    MEM-3 Conditionally display media picker

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Feather } from '@expo/vector-icons'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -15,37 +16,48 @@ export default function HomeScreen() {
     resolver: zodResolver(entrySchema),
   })
 
+  const [selectionStart, setSelectionStart] = useState(0)
+  const [selectionEnd, setSelectionEnd] = useState(0)
+  const isMediaPickerShown = selectionEnd - selectionStart > 0
+
   return (
-    <SafeAreaView className="mx-4">
-      <Controller
-        name="text"
-        control={control}
-        render={({ field: { onChange, value } }) => (
-          <TextInput
-            className="mb-4 font-cp"
-            autoFocus={true}
-            multiline={true}
-            placeholder="Start writing..."
-            selectionColor={colors.gray[500]}
-            value={value}
-            onChangeText={onChange}
-            onSelectionChange={(event) => {
-              const { start, end } = event.nativeEvent.selection
-              if (end - start > 0) console.log(value.substring(start, end))
-            }}
-          />
+    <SafeAreaView>
+      <View className="p-4">
+        <Controller
+          name="text"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              className="mb-4 font-cp"
+              autoFocus={true}
+              multiline={true}
+              placeholder="Start writing..."
+              selectionColor={colors.gray[500]}
+              value={value}
+              onChangeText={onChange}
+              onSelectionChange={(event) => {
+                const { start, end } = event.nativeEvent.selection
+                setSelectionStart(start)
+                setSelectionEnd(end)
+              }}
+            />
+          )}
+        />
+        {isMediaPickerShown && (
+          <View className="flex-row self-start rounded-full border border-gray-700">
+            <TouchableOpacity className="px-4 py-1">
+              <Feather name="image" size={20} color={colors.gray[700]} />
+            </TouchableOpacity>
+            <View className="h-full w-px bg-gray-700" />
+            <TouchableOpacity className="px-4 py-1">
+              <Feather name="video" size={20} color={colors.gray[700]} />
+            </TouchableOpacity>
+            <View className="h-full w-px bg-gray-700" />
+            <TouchableOpacity className="px-4 py-1">
+              <Feather name="mic" size={20} color={colors.gray[700]} />
+            </TouchableOpacity>
+          </View>
         )}
-      />
-      <View className="flex-row border border-gray-400 p-0">
-        <TouchableOpacity>
-          <Feather name="image" size={24} color={colors.gray[700]} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Feather name="video" size={24} color={colors.gray[700]} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Feather name="mic" size={24} color={colors.gray[700]} />
-        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
