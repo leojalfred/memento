7b578522d3426a6df9bec6197f837b92649918a1
Leo Alfred <leojalfred@gmail.com>
2024-09-01 14:15:49 -0500

    MEM-3 Set StatusBar style to dark

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 import '../global.css'
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={DefaultTheme}>
+      <StatusBar style="dark" />
       <Stack screenOptions={{ navigationBarHidden: true }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
