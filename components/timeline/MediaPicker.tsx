e345c6eb92308ccf002ca08d1adf8724cf031464
Leo Alfred <leojalfred@gmail.com>
2024-09-01 15:26:17 -0500

    MEM-4 Support image selection

--- components/timeline/MediaPicker.tsx
+++ components/timeline/MediaPicker.tsx
@@ -1,11 +1,23 @@
 import Divider from '@/components/Divider'
 import IconButton from '@/components/IconButton'
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react'
 import { View } from 'react-native'
 
 export default function MediaPicker() {
+  const [image, setImage] = useState<string | null>(null)
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync()
+    if (!result.canceled) {
+      setImage(result.assets[0].uri)
+      console.log(image)
+    }
+  }
+
   return (
     <View className="flex-row self-start rounded-full border border-gray-700">
-      <IconButton icon="image" />
+      <IconButton icon="image" onPress={pickImage} />
       <Divider />
       <IconButton icon="video" />
       <Divider />
