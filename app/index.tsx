9994923a35f8e5796c519588b629d776dd21e048
Leo Alfred <leojalfred@gmail.com>
2024-09-01 00:51:32 -0500

    MEM-2 Add more input attributes

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { styled } from 'nativewind'
 import { TextInput } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import colors from 'tailwindcss/colors'
 
 const StyledTextInput = styled(TextInput)
 
@@ -9,8 +10,16 @@ export default function HomeScreen() {
     <SafeAreaView>
       <StyledTextInput
         className="mx-4 font-cp"
-        placeholder="Start writing..."
+        autoFocus={true}
         multiline={true}
+        placeholder="Start writing..."
+        selectionColor={colors.gray[500]}
+        onSelectionChange={(event) => {
+          const { selection } = event.nativeEvent
+          if (selection.end - selection.start > 0) {
+            console.log(selection)
+          }
+        }}
       />
     </SafeAreaView>
   )
