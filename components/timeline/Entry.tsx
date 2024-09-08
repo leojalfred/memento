a42c7a7ed88eca11d0ca3c09944f5977487358b8
Leo Alfred <leojalfred@gmail.com>
2024-09-07 23:18:20 -0500

    MEM-10 Refactor attachment spacing

--- components/timeline/Entry.tsx
+++ components/timeline/Entry.tsx
@@ -148,13 +148,20 @@ export default function Entry({
             )
           })
 
-          const shouldHaveZeroLeftMargin =
-            attachment.start === 0 || value[attachment.start - 1] !== ' '
-          const shouldHaveNegativeRightMargin =
-            attachment.end === value.length || value[attachment.end] !== ' '
+          const attachmentStartsInWord = value[attachment.start - 1] !== ' '
+          const attachmentStartsAtStart = attachment.start === 0
+          const attachmentEndsAtEnd = attachment.end === value.length
+          const attachmentEndsInWord = value[attachment.end] !== ' '
+          const attachmentHasAttachmentAsNextWord =
+            i !== sortedAttachments.length - 1 &&
+            attachment.end + 1 === sortedAttachments[i + 1].start
           const classes = twMerge(
-            shouldHaveZeroLeftMargin && 'ml-0',
-            shouldHaveNegativeRightMargin && '-mr-2.5',
+            attachmentStartsInWord && 'ml-0',
+            attachmentStartsAtStart && '-ml-1',
+            (attachmentEndsAtEnd ||
+              attachmentEndsInWord ||
+              attachmentHasAttachmentAsNextWord) &&
+              '-mr-2.5',
           )
 
           acc.push(
