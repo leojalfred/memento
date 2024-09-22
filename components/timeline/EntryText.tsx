c08c06f72687c6472156db31659358f35442d964
Leo Alfred <leojalfred@gmail.com>
2024-09-22 12:40:14 -0500

    MEM-32 Refactor EntryText to use early returns

--- components/timeline/EntryText.tsx
+++ components/timeline/EntryText.tsx
@@ -18,55 +18,65 @@ export default function EntryText({
   scrollY,
 }: EntryTextProps) {
   return useMemo(() => {
-    return sortedAttachments.length > 0 ? (
-      <View className="-ml-2.5 flex-row flex-wrap">
-        {sortedAttachments.reduce<React.ReactNode[]>((acc, attachment, i) => {
-          const previousText = value
-            .slice(i > 0 ? sortedAttachments[i - 1].end : 0, attachment.start)
-            .trim()
-          previousText.split(' ').forEach((word, j) => {
-            acc.push(
-              <Text
-                key={`previous-${attachment.start}-${attachment.end}-${j}`}
-                className="font-cp -z-[1] ml-2.5 leading-[1.3125] text-zinc-900 dark:text-zinc-100"
-              >
-                {word}
-              </Text>,
-            )
-          })
-
-          acc.push(
-            <Attachment
-              key={`attachment-${attachment.start}-${attachment.end}`}
-              i={i}
-              value={value}
-              attachment={attachment}
-              sortedAttachments={sortedAttachments}
-              isEditing={isEditing}
-              scrollY={scrollY}
-            />,
-          )
-
-          if (i === sortedAttachments.length - 1) {
-            const remainingText = value.slice(attachment.end).trim()
-            remainingText.split(' ').forEach((word, j) => {
+    if (sortedAttachments.length > 0)
+      return (
+        <View className="-ml-2.5 flex-row flex-wrap">
+          {sortedAttachments.reduce<React.ReactNode[]>((acc, attachment, i) => {
+            const previousText = value
+              .slice(i > 0 ? sortedAttachments[i - 1].end : 0, attachment.start)
+              .trim()
+            previousText.split(' ').forEach((word, j) => {
               acc.push(
                 <Text
-                  key={`remaining-${attachment.start}-${attachment.end}-${j}`}
+                  key={`previous-${attachment.start}-${attachment.end}-${j}`}
                   className="font-cp -z-[1] ml-2.5 leading-[1.3125] text-zinc-900 dark:text-zinc-100"
                 >
                   {word}
                 </Text>,
               )
             })
-          }
 
-          return acc
-        }, [])}
-      </View>
-    ) : (
-      <Text className="font-cp leading-[1.3125] text-zinc-900 dark:text-zinc-100">
-        {value}
+            acc.push(
+              <Attachment
+                key={`attachment-${attachment.start}-${attachment.end}`}
+                i={i}
+                value={value}
+                attachment={attachment}
+                sortedAttachments={sortedAttachments}
+                isEditing={isEditing}
+                scrollY={scrollY}
+              />,
+            )
+
+            if (i === sortedAttachments.length - 1) {
+              const remainingText = value.slice(attachment.end).trim()
+              remainingText.split(' ').forEach((word, j) => {
+                acc.push(
+                  <Text
+                    key={`remaining-${attachment.start}-${attachment.end}-${j}`}
+                    className="font-cp -z-[1] ml-2.5 leading-[1.3125] text-zinc-900 dark:text-zinc-100"
+                  >
+                    {word}
+                  </Text>,
+                )
+              })
+            }
+
+            return acc
+          }, [])}
+        </View>
+      )
+
+    if (value.length > 0)
+      return (
+        <Text className="font-cp leading-[1.3125] text-zinc-900 dark:text-zinc-100">
+          {value}
+        </Text>
+      )
+
+    return (
+      <Text className="font-cp leading-[1.3125] text-zinc-500 dark:text-zinc-600">
+        Start writing...
       </Text>
     )
   }, [sortedAttachments, value, isEditing, scrollY])
